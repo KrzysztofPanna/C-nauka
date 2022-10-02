b09357b7a2a7f88aadaf7d71b30bffe0e75683a4
Krzysztof Panna <krzysztof.panna1@gmail.com>
2022-10-02 14:25:54 +0200

    dodanie cwiczenia petla whle

--- 05_petla_while/main.cpp
+++ 05_petla_while/main.cpp
@@ -2,8 +2,19 @@
 
 using namespace std;
 
+int limit, bacteries=1, time=0;
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    cout << "How many bacteries to dye" << endl;
+    cin>>limit;
+
+    while( bacteries<=limit)
+    {
+        bacteries=bacteries*2;
+        time++;
+        cout<<"You will dye in "<<time<<" hours.";
+        cout<<" You will have "<< bacteries<<" bacteries"<<endl;
+    }
     return 0;
 }
