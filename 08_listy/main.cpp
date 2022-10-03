c67436c4654d1b9c76430b0a991ac381c05e3c74
Krzysztof Panna <krzysztof.panna1@gmail.com>
2022-10-04 00:10:29 +0200

    dodanie kodu z listami

--- 08_listy/main.cpp
+++ 08_listy/main.cpp
@@ -2,8 +2,23 @@
 
 using namespace std;
 
+float greed[5];
+float suma = 0,ave;
+
 int main()
 {
-    cout << "Hello world!" << endl;
+
+
+    for(int i=0;i<5;i++)
+    {
+        cout << "Write "<<i+1<<" marks" << endl;
+        cin>>greed[i];
+        suma +=greed[i];    //suma +=suma+greed[i];
+    }
+    ave = suma/5;
+
+    cout<<suma<<"\n";
+    cout<<"this is average "<<ave;
+
     return 0;
 }
